5bbcc50cfd5ec3233192839c4d4a02b341c0e2f4
grtrz <grant.terz@gmail.com>
2023-10-23 22:56:34 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+from sentence_transformers import SentenceTransformer
+import streamlit as st
+
+df = pd.read_csv('qa.csv')
+questions = df['Вопрос']
+answers = df['Ответ']
+
+embeddings = np.load('embeddings.npy')
+model = SentenceTransformer('all-MiniLM-L6-v2')
+
+user_input = st.text_input('Введите вопрос:')
+emb = model.encode(user_input)
+cosine_scores = np.dot(emb, embeddings.T) / (np.linalg.norm(emb) * np.linalg.norm(embeddings, axis=1))
+top_k = 3
+top_k_idx = np.argsort(cosine_scores)[::-1][:top_k]
+selected_answers = []
+if user_input != '':
+    for idx, i in zip(top_k_idx, range(1, top_k+1)):
+        st.write(f'{i}. Вопрос: {questions[idx]}')
+        selected_answers.append(answers[idx])
+    idx = st.selectbox('Выберите номер вопроса', range(1, top_k+1)) - 1
+    st.write(f'Ответ: {selected_answers[idx]}')
